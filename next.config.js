21d7dab404ff517f4c93592d08ec073fc3d81076
windtail16 <windtail16@naver.com>
2025-01-05 20:19:52 +0900

    feat: VanillaExtract 설치

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+const withVanillaExtract = createVanillaExtractPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+};
+
+module.exports = withVanillaExtract(nextConfig);
